d7545523d449c69d810685a5169e32de56afa5ba
David Sklar <davidhsklar@gmail.com>
2022-02-28 19:56:21 -0500

    added basic functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,26 @@
-window.alert('this is an alert! Javascript is running');
+// to create a custom function use this code and replace fight
+
+var playerName = window.prompt("What is your robot's name?");
+
+var playerName = "David";
+
+// prompt that gets answered and logged for dev side
+console.log(playerName);
+
+console.log("This leaves a string, good for leaving a message");
+console.log(10+10);
+console.log("Our robots name is"  +  playerName);
+
+// Ready for Battle
+
+console.log(playerName + "is ready for battle!");
+
+// Your Robot has won!
+
+console.log ("Your Robot"  +  playerName  +  "has won!");
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
